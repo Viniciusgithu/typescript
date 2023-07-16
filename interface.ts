6b82fd92f9809b3db1fee210c1efd59e72f77dcc
Viniciusgithu <viniciuslimavirtuel@email.com>
2023-07-16 19:04:17 -0300

    doc:typescript-02

--- interface.ts
+++ interface.ts
@@ -1,14 +1,43 @@
 //Declarando interface 
+//Geralmente utilizada para definir objetos
 
-
-interface User {
-  id: number,
-  name: string,
-  email?: string,
+interface Empresa {
+  nome: string;
+  fundacao: number;
+  pais: string;
 }
 
-let newUser: User = {
-  id: 500,
-  name: "Kronos",
-  email: "kronos@gmail.com"
+interface Product {
+  nome: string;
+  preco: number;
+  descricao: string;
+  garantia: string;
+  seguroAcidente: boolean;
+  empresaFabricante: Empresa;
+  empresaMontadora: Empresa;
+}
+
+
+async function fetchProduct() {
+  const response = await fetch(`https://api.origamid.dev/json/notebook.json`)
+  const data = await response.json()
+  showProduct(data)
+}
+
+fetchProduct()
+
+
+function showProduct(data: Product) {
+  document.body.innerHTML = `
+    <div>
+      <h2>${data.nome}</h2>
+      <p>R$ ${data.preco}</p>
+      <div>
+        <h3>Fabricante: ${data.empresaFabricante.nome}</h3>
+      </div>
+      <div>
+        <h3>Montadora: ${data.empresaMontadora.nome}</h3>
+      </div>
+    </div>
+  `;
 }
